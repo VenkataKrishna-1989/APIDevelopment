d6dc08612581caac267ff3763ae313873f042689
VenkataKrishna-1989 <dotnetvvk@outlook.com>
2025-12-26 18:48:09 +0530

    Changes for v2

---
+++ NZWalksAPI/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("FetchProducts")]
+        public IActionResult FetchProducts()
+        {
+            return Ok(new { ProductId = 1,Name="TV",Category="Electronics" } );
+        }
+    }
+}
